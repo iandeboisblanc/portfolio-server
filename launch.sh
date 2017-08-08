900b3f1ccd8cf5bd26a404254f107e62e9da8808
Ian deBoisblanc <iandeboisblanc@gmail.com>
2017-08-08 13:55:55 -0700

    Add shell script for launching app

---
+++ launch.sh
@@ -0,0 +1,12 @@
+npm install
+rm -r portfolio
+git clone https://github.com/iandeboisblanc/portfolio
+cd portfolio
+npm install
+cd ..
+rm -r evolution
+git clone https://github.com/iandeboisblanc/evolution
+npm install
+cd ..
+npm run build
+npm start
